8aa22d48a8b5a78d1fbeec9a935537cf1ff28c35
Neverender <tldavis09@yahoo.com>
2020-02-02 20:56:09 -0600

    added 5 day forecast w/ ajax

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,25 @@
-var apiKey = "a9ef19731f63676098dba0a6b2b8502e";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var apiKey = "&appId=a9ef19731f63676098dba0a6b2b8502e";
+var nowUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var queryCity = "Dallas";
 
 var date = Date();
 console.log(date);
 
 $.ajax({
-	url: queryURL + queryCity + "&appId=" + apiKey,
+	url: nowUrl + queryCity + apiKey,
 	method: "GET"
-}).then(function(results) {
-	console.log(results);
+}).then(function(response) {
+	console.log(response);
+
+	console.log(nowUrl + queryCity + apiKey);
+});
+
+$.ajax({
+	url: forecastUrl + queryCity + apiKey,
+	method: "GET"
+}).then(function(response) {
+	console.log(response);
+
+	console.log(forecastUrl + queryCity + apiKey);
 });
