5f47a14f1302a96198756215adb9f429f33e3a3c
Neverender <tldavis09@yahoo.com>
2020-02-03 21:16:05 -0600

    .

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
-$cityName = $(".city-name");
+$cityName = $("#city-name");
+$searchBtn = $("#search-button");
+$searchInput = $("#search-input");
+
+
+// API Variables
 var apiKey = "&appId=a9ef19731f63676098dba0a6b2b8502e";
 var nowUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?";
-var queryCity = "Dallas";
+var queryCity = $searchInput.val()
+
+console.log(queryCity;
 
 var date = new Date().toLocaleDateString("en-US");
-console.log(date);
+
 
 $.ajax({
 	url: nowUrl + queryCity + apiKey,
@@ -33,25 +40,21 @@ $.ajax({
 	var windSpeed = response.wind.speed;
 	var icon = response.weather[0].icon;
 
-	// console.log(cityName);
-	// console.log(temperature);
-	// console.log(humidity);
-	// console.log(windSpeed);
-	// console.log(icon);
-	// console.log(nowUrl + queryCity + apiKey);
+	console.log(cityName);
+	console.log(temperature);
+	console.log(humidity);
+	console.log(windSpeed);
+	console.log(icon);
+	console.log(nowUrl + queryCity + apiKey);
 
-	// $cityName.text(icon);
+	$cityName.text(icon);
 });
 
-// $.ajax({
-// 	url: forecastUrl + queryCity + apiKey,
-// 	method: "GET"
-// }).then(function(response) {
-// 	console.log(response);
+$.ajax({
+	url: forecastUrl + queryCity + apiKey,
+	method: "GET"
+}).then(function(response) {
+	console.log(response);
 
-// 	console.log(forecastUrl + queryCity + apiKey);
-// });
-
-// api.openweathermap.org/data/2.5/uvi?
-
-// lat=37.75&lon=-122.37
+	console.log(forecastUrl + queryCity + apiKey);
+});
