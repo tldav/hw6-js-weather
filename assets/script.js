404b06f11ec50c3ed9a944fb0a7eb342b3f4ebe8
Neverender <tldavis09@yahoo.com>
2020-02-05 20:16:28 -0600

    Fixed the weather li appending issue. Current weather and uv index should work with more than one consecutive search now.

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,75 @@
-$cityName = $("#city-name");
+// DOM Variables
+$cityDisplay = $("#city-name");
 $searchBtn = $("#search-button");
 $searchInput = $("#search-input");
-
+$weatherStage = $("#weather-stage");
+$weatherUl = $("#weather-ul");
+$tCurrent = $("#t-li");
+$hCurrent = $("#h-li");
+$wCurrent = $("#w-li");
+$uCurrent = $("#u-li");
+$weatherIcon = $("#icon");
 
 // API Variables
 var apiKey = "&appId=a9ef19731f63676098dba0a6b2b8502e";
 var nowUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?";
-var queryCity = $searchInput.val()
+var queryCity = $searchInput.val();
 
-console.log(queryCity;
-
+// Current date/time stored in a variable
 var date = new Date().toLocaleDateString("en-US");
 
-
-$.ajax({
-	url: nowUrl + queryCity + apiKey,
-	method: "GET"
-}).then(function(response) {
-	var longitude = response.coord.lon;
-	console.log(longitude);
-	var latitude = response.coord.lat;
-	console.log(latitude);
+$searchBtn.on("click", function() {
+	// Set the city name in the API url to the user inputted city
+	queryCity = $searchInput.val();
 
 	$.ajax({
-		url: uvUrl + "lat=" + latitude + "&lon=" + longitude + apiKey,
+		url: nowUrl + queryCity + apiKey,
 		method: "GET"
-	}).then(function(uvResponse) {
-		var uvIndex = uvResponse.value;
-		console.log(uvIndex);
+	}).then(function(response) {
+		// Targets city and weather info from the JSON object
+		var cityName = response.name;
+		var temperature = (response.main.temp - 273.15) * 1.8 + 32;
+		temperature = Math.floor(temperature);
+		var humidity = response.main.humidity;
+		var windSpeed = response.wind.speed;
+		// var iconCode = response.weather[0].icon;
+		// var iconUrl =
+		// 	"http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+
+		// Displays the current city and weather based on user input
+		$cityDisplay.text(cityName + " " + date);
+		// $weatherIcon.attr("src", iconUrl);
+		$tCurrent.text("Temperature: " + temperature + " \u00B0F");
+		$hCurrent.text("Humidity: " + humidity + "%");
+		$wCurrent.text("Wind Speed: " + windSpeed + " MPH");
+
+		// Stores longitude and latitude from first ajax call into variables to be used in the UV ajax call
+		var longitude = response.coord.lon;
+		var latitude = response.coord.lat;
+		// UV index ajax call
+		$.ajax({
+			url: uvUrl + "lat=" + latitude + "&lon=" + longitude + apiKey,
+			method: "GET"
+		}).then(function(uvResponse) {
+			// Stores and displays the UV index with the current weather
+			var uvIndex = uvResponse.value;
+			$uCurrent.text("UV Index: " + uvIndex);
+		});
 	});
 
-	console.log(response);
-
-	var cityName = response.name;
-	var temperature = (response.main.temp - 273.15) * 1.8 + 32;
-	var humidity = response.main.humidity;
-	var windSpeed = response.wind.speed;
-	var icon = response.weather[0].icon;
-
-	console.log(cityName);
-	console.log(temperature);
-	console.log(humidity);
-	console.log(windSpeed);
-	console.log(icon);
-	console.log(nowUrl + queryCity + apiKey);
-
-	$cityName.text(icon);
+	// Displays the weather
+	$weatherStage.removeClass("hidden");
+	forecast();
 });
 
-$.ajax({
-	url: forecastUrl + queryCity + apiKey,
-	method: "GET"
-}).then(function(response) {
-	console.log(response);
-
-	console.log(forecastUrl + queryCity + apiKey);
-});
+function forecast() {
+	$.ajax({
+		url: forecastUrl + queryCity + apiKey,
+		method: "GET"
+	}).then(function(response) {
+		queryCity = $searchInput.val();
+		console.log(response);
+	});
+}
